d3ae330207dedca7e23db6fd4971d2dcbbc05f89
LastValentina <ivanovavv9@gmail.com>
2021-02-09 23:54:15 +0300

    hometask_3

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -17,18 +17,29 @@ public class Driver implements IDriver {
         return appiumDriver;
     }
 
-    public Driver(String platformName, String deviceName, String browserName, String app) {
+    public Driver(String platformName, String deviceName, String udid, String browserName,
+                  String app, String appPackage, String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        // mandatory Android capabilities
         capabilities.setCapability("platformName", platformName);
         capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("udid", udid);
         if (app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
         capabilities.setCapability("browserName", browserName);
         capabilities.setCapability("chromedriverDisableBuildCheck", "true");
+        // Capabilities for test of Android native app on EPAM Mobile Cloud
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+        // Capabilities for test of iOS native app on EPAM Mobile Cloud
+        capabilities.setCapability("bundleId", bundleId);
+        if (platformName.equals("iOS")) capabilities.setCapability("automationName", "XCUITest");
+
         try {
             appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        // Timeouts tuning
         appiumDriver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
     }
 }
